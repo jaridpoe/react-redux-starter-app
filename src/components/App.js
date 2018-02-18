00390d0654afbdedf9a91e5bc975289cf4aa6ada
Jarid Poe <jaridpoe@gmail.com>
2018-02-18 08:52:28 -0600

    Add NoPageFound component
    When user navigate to an incorrect url this page will display.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import {
+  Route,
+  Switch
+} from 'react-router-dom'
 
 import Navbar from './../components/common/Navbar/views/NavbarView.js'
 import HomePage from './../components/HomePage/container/HomePageContainer.js'
 import SearchPage from './../components/SearchPage/containers/SearchPageContainer'
 import SettingPage from './../components/SettingPage/container/SettingPageContainer'
+import {NoPageFound} from "./common/NoPageFound/views/NoPageFoundView"
 
 /*
 React Router had dynamic routing which takes place during render, not as a configurable file outside like v3
@@ -15,9 +19,14 @@ const App = () => (
     <Navbar/>
     <div className="container-fluid">
       <div className="row">
-        <Route exact path={`/`} component={HomePage}/>
-        <Route path={`/search`} component={SearchPage}/>
-        <Route path={`/settings`} component={SettingPage}/>
+
+        <Switch>
+          <Route exact path={`/`} component={HomePage}/>
+          <Route path={`/search`} component={SearchPage}/>
+          <Route path={`/settings`} component={SettingPage}/>
+          <Route component={NoPageFound}/>
+        </Switch>
+
       </div>
     </div>
 
